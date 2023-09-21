542c0c967d5190384ee0c7d37f0872445fc05476
Malak-louki <bouaker.malak@outlook.fr>
2023-09-21 19:03:51 +0200

    fix migration 2

--- migrations/Version20230921170112.php
+++ migrations/Version20230921170112.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20230920120558 extends AbstractMigration
+final class Version20230921170112 extends AbstractMigration
 {
     public function getDescription(): string
     {
