3361178b920e47a1de309293980100cbc5fb6a08
huekhuc <khuchue.hus@gmail.com>
2023-10-24 05:41:48 +0200

    feat: tournament registration - getCollection for user

---
+++ src/Controller/TournamentRegistration/GetCollectionUser.php
@@ -0,0 +1,55 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Controller\TournamentRegistration;
+
+use App\Entity\TournamentRegistration;
+use App\Exception\NotFound;
+use App\ObjectManipulation\TransferObject;
+use App\Repository\TournamentRegistrationRepository;
+use OpenApi\Attributes as OA;
+use App\Dto\TournamentRegistrationOutput;
+use Nelmio\ApiDocBundle\Annotation\Model;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+class GetCollectionUser extends AbstractController
+{
+    #[Route('/api/users/{uuid}/tournament-registrations', methods: ['GET'])]
+    #[OA\Response(
+        response: 200,
+        description: 'Returns the information of all tournament registrations of user',
+        content: new OA\JsonContent(
+            type: 'array',
+            items: new OA\Items(ref: new Model(type: TournamentRegistrationOutput::class))
+        )
+    )]
+    #[OA\Tag(name: 'Tournament Registration')]
+    public function __invoke(
+        string $uuid,
+        TournamentRegistrationRepository $tournamentRegistrationRepository,
+        TournamentRegistrationOutput $tournamentRegistrationOutput,
+        TransferObject $transferDTO,
+    ): Response {
+        $tournamentRegistrations = $tournamentRegistrationRepository->findByUserUuid($uuid);
+
+        if (count($tournamentRegistrations) === 0) {
+            throw new NotFound('Tournament Registration not found');
+        }
+
+        $tournamentRegistrationsOutput = [];
+
+        foreach ($tournamentRegistrations as $tournamentRegistration) {
+            // assert($tournamentRegistration instanceof TournamentRegistration);
+            $tournamentRegistrationOutput = new TournamentRegistrationOutput();
+
+            $transferDTO($tournamentRegistration, $tournamentRegistrationOutput);
+
+            $tournamentRegistrationsOutput[] = $tournamentRegistrationOutput;
+        }
+
+        return $this->json($tournamentRegistrationsOutput, 200);
+    }
+}
--- src/Repository/TournamentRegistrationRepository.php
+++ src/Repository/TournamentRegistrationRepository.php
@@ -5,8 +5,8 @@ declare(strict_types=1);
 namespace App\Repository;
 
 use App\Entity\TournamentRegistration;
-use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
 use Doctrine\Persistence\ManagerRegistry;
+use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
 
 /**
  * @extends ServiceEntityRepository<TournamentRegistration>
@@ -16,5 +16,33 @@ class TournamentRegistrationRepository extends ServiceEntityRepository
     public function __construct(ManagerRegistry $registry)
     {
         parent::__construct($registry, TournamentRegistration::class);
+    }
+
+    /**
+    * @return TournamentRegistration[]
+    */
+    public function findByUserUuid(string $uuid): array
+    {
+        $qb = $this->createQueryBuilder('tournamentRegistration')
+            ->select('tournamentRegistration')
+            ->innerJoin('App\Entity\User', 'user', 'WITH', 'user = tournamentRegistration.user')
+            ->where('user.uuid = :userUuid')
+            ->setParameter('userUuid', $uuid)
+            ->orderBy('tournamentRegistration.createdAt', 'DESC');
+
+        $query = $qb->getQuery();
+
+        $results = (array) $query->getResult();
+
+        // if (count($results) === 0) {
+        //     return [];
+        // }
+
+        // foreach ($results as $result) {
+        //     assert($result instanceof TournamentRegistration);
+        // }
+
+        return $results;
+
     }
 }
