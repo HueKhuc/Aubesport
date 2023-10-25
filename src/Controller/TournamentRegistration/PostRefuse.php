da1a085009bfc291daaaeb05c115b85f0095a2b5
huekhuc <khuchue.hus@gmail.com>
2023-10-25 15:48:09 +0200

    feat: tournament registration - post refuse

---
+++ src/Controller/TournamentRegistration/PostRefuse.php
@@ -0,0 +1,65 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Controller\TournamentRegistration;
+
+use App\Exception\Conflict;
+use App\Exception\NotFound;
+use OpenApi\Attributes as OA;
+use App\Entity\TournamentRegistration;
+use Doctrine\ORM\EntityManagerInterface;
+use App\Dto\TournamentRegistrationOutput;
+use Nelmio\ApiDocBundle\Annotation\Model;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use App\Event\RefusedTournamentRegistrationEvent;
+use Symfony\Component\EventDispatcher\EventDispatcherInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\ObjectManipulation\TransferTournamentRegistrationToOutput;
+
+class PostRefuse extends AbstractController
+{
+    #[Route('/api/tournament-registrations/{uuid}/refuse', methods: ['POST'])]
+    #[OA\Response(
+        response: 201,
+        description: 'Refuse a tournament registration',
+        content: new OA\JsonContent(
+            type: 'array',
+            items: new OA\Items(ref: new Model(type: TournamentRegistrationOutput::class))
+        )
+    )]
+    #[OA\Tag(name: 'TournamentRegistration')]
+    public function __invoke(
+        string $uuid,
+        EntityManagerInterface $entityManager,
+        TournamentRegistrationOutput $tournamentRegistrationOutput,
+        TransferTournamentRegistrationToOutput $transferTournamentRegistrationOutput,
+        EventDispatcherInterface $dispatcher,
+    ): Response {
+        $tournamentRegistration = $entityManager->getRepository(TournamentRegistration::class)->find($uuid);
+
+        if ($tournamentRegistration === null) {
+            throw new NotFound('Tournament registration not found');
+        }
+
+        if ($tournamentRegistration->getStatus() !== 'pending') {
+            if ($tournamentRegistration->getStatus() === 'accepted') {
+                throw new Conflict('Tournament registration has been accepted');
+            } else {
+                throw new Conflict('Tournament registration has been refused');
+            }
+        }
+
+        $tournamentRegistration->setStatus('refused');
+
+        $entityManager->flush();
+
+        $event = new RefusedTournamentRegistrationEvent($tournamentRegistration);
+        $dispatcher->dispatch($event, RefusedTournamentRegistrationEvent::NAME);
+
+        $transferTournamentRegistrationOutput($tournamentRegistration, $tournamentRegistrationOutput);
+
+        return $this->json($tournamentRegistrationOutput, 201);
+    }
+}
