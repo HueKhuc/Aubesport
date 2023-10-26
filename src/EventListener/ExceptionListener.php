57256a5ceab63fe34c8b74e9bcd41d796b729229
huekhuc <khuchue.hus@gmail.com>
2023-10-26 03:16:30 +0200

    feat: access control for different roles - refactor: ExceptionListener

--- src/EventListener/ExceptionListener.php
+++ src/EventListener/ExceptionListener.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace App\EventListener;
 
+use Throwable;
 use App\Exception\Conflict;
 use App\Exception\NotFound;
 use Psr\Log\LoggerInterface;
@@ -12,7 +13,9 @@ use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\Serializer\SerializerInterface;
 use Symfony\Component\HttpKernel\Event\ExceptionEvent;
 use Symfony\Component\EventDispatcher\Attribute\AsEventListener;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 use Symfony\Component\Validator\Exception\ValidationFailedException;
+use Symfony\Component\HttpKernel\Exception\AccessDeniedHttpException;
 
 #[AsEventListener(event: KernelEvents::EXCEPTION)]
 class ExceptionListener
@@ -26,29 +29,48 @@ class ExceptionListener
     public function __invoke(ExceptionEvent $exceptionEvent): void
     {
         $exception = $exceptionEvent->getThrowable();
-        $previous = $exception->getPrevious();
+        $statusCode = $this->getStatusCodeForException($exception);
+        $message = $this->getMessageForException($exception);
 
-        if ($previous instanceof ValidationFailedException) {
-            $response = new JsonResponse(
-                $this->serializer->serialize($previous->getViolations(), 'json'),
-                422,
-                json: true
-            );
-        } elseif ($exception instanceof Conflict) {
-            $response = new JsonResponse(
-                ['message' => $exception->getMessage()],
-                409
-            );
-            $this->logger->error($exception->getMessage(), ['trace' => $exception->getTrace()]);
+
+        if ($exception->getPrevious() instanceof ValidationFailedException) {
+            $response = new JsonResponse($message, $statusCode, json: true);
         } else {
-            $code = ($exception instanceof NotFound) ? 404 : 500;
-            $response = new JsonResponse(
-                ['message' => $exception->getMessage()],
-                $code
-            );
-            $this->logger->error($exception->getMessage(), ['trace' => $exception->getTrace()]);
+            $response = new JsonResponse(['message' => $message], $statusCode);
+        }
+
+        if ($statusCode === 500) {
+            $this->logger->error($message, ['trace' => $exception->getTrace()]);
         }
 
         $exceptionEvent->setResponse($response);
+    }
+
+    private function getStatusCodeForException(Throwable $exception): int
+    {
+        switch (true) {
+            case $exception->getPrevious() instanceof ValidationFailedException:
+                return 422;
+            case $exception instanceof Conflict:
+                return 409;
+            case $exception instanceof AccessDeniedHttpException:
+                return 403;
+            case $exception instanceof NotFound:
+            case $exception instanceof NotFoundHttpException:
+                return 404;
+            default:
+                return 500;
+        }
+    }
+
+    private function getMessageForException(Throwable $exception): string
+    {
+        $previous = $exception->getPrevious();
+
+        if ($previous instanceof ValidationFailedException) {
+            return $this->serializer->serialize($previous->getViolations(), 'json');
+        } else {
+            return $exception->getMessage();
+        }
     }
 }
